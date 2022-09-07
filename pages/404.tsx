84dcbc0cdbb6274608dfafa4939a0202a2f5a89e
Serge <best-M9lTHblu@ya.ru>
2022-09-07 21:40:14 +0300

    delete README

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,4 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
 const Error = () => {
-  const router = useRouter()
-
-  useEffect(() => {
-    setTimeout(() => {
-      router.push('/')
-    }, 3000)
-  }, [])
 
   return (
     <div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import 'the-new-css-reset/css/reset.css'
 import '../styles/globals.scss'
 
-import { Layout } from '../components/Layout/Layout'
+import { Layout } from '../components/Layout'
 import { AppProps } from 'next/app'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
